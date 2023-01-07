3461f08d8335779a810c1bdfa958fd7f768e3ab5
AndriyKy <akrechovetskiy@gmail.com>
2023-01-07 17:09:20 +0200

    init: Registration of URLs for application books

--- library_api/urls.py
+++ library_api/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users/", include("users.urls", namespace="users")),
+    path("books/", include("books.urls", namespace="books")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]

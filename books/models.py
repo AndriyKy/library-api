3e30475e8122d43c1c5e4065511a08d62d1f84c0
AndriyKy <akrechovetskiy@gmail.com>
2023-01-07 12:36:04 +0200

    db: Initialize Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverOfBook(models.TextChoices):
+        HARD = "H"
+        SOFT = "F"
+
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    cover = models.CharField(max_length=1, choices=CoverOfBook.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def get_cover(self):
+        # Get label from choices enum
+        cover_index = self.CoverOfBook.values.index(self.cover)
+        return self.CoverOfBook.labels[cover_index]

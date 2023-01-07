4788297b3d3193855b3291f475682d1f01965e36
AndriyKy <akrechovetskiy@gmail.com>
2023-01-07 10:45:16 +0200

    init: Registration of URLs for application users

--- library_api/urls.py
+++ library_api/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("users/", include("users.urls", namespace="users")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
